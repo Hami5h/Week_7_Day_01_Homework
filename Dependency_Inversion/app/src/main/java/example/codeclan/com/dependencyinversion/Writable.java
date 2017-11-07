72e01c426bf5918fe4e80c2cc9558c4ffa4d9d6d
Hamish <hamishfpso@gmail.com>
2017-11-07 08:20:57 +0000

    Some refactoring done, but bear can't write

---
+++ Dependency_Inversion/app/src/main/java/example/codeclan/com/dependencyinversion/Writable.java
@@ -0,0 +1,10 @@
+package example.codeclan.com.dependencyinversion;
+
+/**
+ * Created by hamishstewart on 06/11/2017.
+ */
+
+public interface Writable {
+    void write(String text);
+
+}
---
+++ Dependency_Inversion/app/src/test/java/example/codeclan/com/dependencyinversion/BearTest.java
@@ -0,0 +1,41 @@
+package example.codeclan.com.dependencyinversion;
+
+/**
+ * Created by user on 27/06/2017.
+ */
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+public class BearTest{
+
+    Bear bear;
+    Diary diary;
+
+
+   @Test
+    public void testWriteToDiary(){
+       Diary diary = new Diary();
+        diary.write("Dear diary, mood: apathetic...");
+        assertEquals("Dear diary, mood: apathetic...", diary.readLast());
+    }
+
+    @Test
+    public void hasSomethingToWriteOn() {
+        Bear bear = new Bear(diary);
+        assertEquals(diary, bear.getInstrumentToWriteOn());
+    }
+
+//    @Test
+//    public  void testBearWritesToDiary() {
+//        Diary diary = new Diary();
+//        Bear bear = new Bear(diary);
+//        bear.write("I can write");
+//        assertEquals("I can write", diary.readLast());
+//    }
+
+
+}
